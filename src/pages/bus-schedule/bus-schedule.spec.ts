f10800fbefce5f48ed95c3abbf1285708af90d2c
William R. Arellano <william.arellano@yachaytech.edu.ec>
2018-04-03 22:24:37 -0500

    Added more test

--- src/pages/bus-schedule/bus-schedule.spec.ts
+++ src/pages/bus-schedule/bus-schedule.spec.ts
@@ -1,4 +1,4 @@
-import { Entry, Day } from "./bus-schedule";
+import { Entry, Day, Schedule } from "./bus-schedule";
 /**
    Test for Schedule Entry
  */
@@ -11,4 +11,26 @@ describe("Test for Schedule Entry", ()=>{
         });
 
     });
+});
+
+/**
+   Test for Schedule
+*/
+
+describe("Test for a whole Schedule", ()=>{
+    let entry1 = new Entry(Day.Mon, "Ibarra", "14:00");
+    let entry2 = new Entry(Day.Tues, "Ibarra", "14:05");
+    let entry3 = new Entry(Day.Wed, "Ibarra", "14:10");
+    let entry4 = new Entry(Day.Thur, "Ibarra", "14:15");
+    let entry5 = new Entry(Day.Fri, "Ibarra", "14:20");
+    let entry6 = new Entry(Day.Sat, "Ibarra", "14:25");
+
+    describe("Test for the constructor of Schedule", ()=>{
+        let schedule = new Schedule([entry1, entry2, entry3, entry4, entry5, entry6]);
+        it("should return []", ()=>{
+            expect(schedule.sunday).toEqual([]);
+        });
+    });
+
+
 });
--- src/pages/bus-schedule/bus-schedule.ts
+++ src/pages/bus-schedule/bus-schedule.ts
@@ -1,30 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the BusSchedulePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
-@Component({
-    selector: 'page-bus-schedule',
-    templateUrl: 'bus-schedule.html',
-})
-export class BusSchedulePage {
-    busSchedule: Entry[];
-
-    constructor(public navCtrl: NavController, public navParams: NavParams) {
-    }
-
-    ionViewDidLoad() {
-        console.log('ionViewDidLoad BusSchedulePage');
-    }
-
-    getBusSchedule(){}
-}
 
 /**
    Enum Type for days of the week
@@ -53,13 +29,12 @@ export class Entry{
         this.location = location;
         this.time = time;
     }
-
     /**
        Returns the day of the week of the Entry as a human readable string English
      */
     getDayEn(){
         var dayString;
-        switch(+this.day){
+        switch(this.day){
             case Day.Mon:
                 dayString="Monday";
                 break;
@@ -85,4 +60,76 @@ export class Entry{
         return dayString;
     }
 
+}
+
+/**
+   A schedule is a collection of Entries
+ */
+
+export class Schedule {
+    monday: Entry[] = [];
+    tuesday: Entry[] = [];
+    wednesday: Entry[] = [];
+    thursday: Entry[] = [];
+    friday:  Entry[] = [];
+    saturday: Entry[] = [];
+    sunday: Entry[] = [];
+
+    /**
+       When creating a schedule it receives a list of entries and then it separates the
+       entries by day of the week.
+     */
+    constructor(entries: Entry[]){
+        for(let entry of entries){
+            switch(entry.day){
+                case Day.Mon:
+                    this.monday.push(entry);
+                    break;
+                case Day.Tues:
+                    this.tuesday.push(entry);
+                    break;
+                case Day.Wed:
+                    this.wednesday.push(entry);
+                    break;
+                case Day.Thur:
+                    this.thursday.push(entry);
+                    break;
+                case Day.Fri:
+                    this.friday.push(entry);
+                    break;
+                case Day.Sat:
+                    this.saturday.push(entry);
+                    break;
+                case Day.Sun:
+                    this.sunday.push(entry);
+                    break;
+            }
+        }
+    }
+}
+
+
+/**
+ * Generated class for the BusSchedulePage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+    selector: 'page-bus-schedule',
+    templateUrl: 'bus-schedule.html',
+})
+export class BusSchedulePage {
+    busSchedule: Entry[];
+
+    constructor(public navCtrl: NavController, public navParams: NavParams) {
+    }
+
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad BusSchedulePage');
+    }
+
+    getBusSchedule(){}
 }
